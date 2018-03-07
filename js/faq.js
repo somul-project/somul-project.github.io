f0df7966aa792cbda70d948c9861d96b1a3a1e38
clucle <wjdenwls123@gmail.com>
2018-03-07 09:18:35 +0900

    style: clean code

--- js/faq.js
+++ js/faq.js
@@ -1,34 +1,37 @@
 
-const app = document.getElementById("app");
+var app = document.getElementById("app");
 
 // TODO: url must be changed when deploy main server
-const jsonUrl = "https://raw.githubusercontent.com/clucle/somul-project.github.io/feature/md_to_html/web-inf/faq.json";
+var jsonUrl = "https://raw.githubusercontent.com/clucle/somul-project.github.io/feature/md_to_html/web-inf/faq.json";
 
 var getJSON = function(url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
     xhr.responseType = 'json';
+
     xhr.onload = function() {
-    var status = xhr.status;
-    if (status === 200) {
-        callback(null, xhr.response);
-    } else {
-        allback(status, xhr.response);
-    }
+        var status = xhr.status;
+        if (status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status, xhr.response);
+        }
     };
     xhr.send();
 };
 
-function renderJson(url) {
+function renderByJsonUrl(url) {
+    
     getJSON(url, function(err, data) {
         if (err !== null) {
-            console.log("load json file : " + err);
         } else {
-            console.log(data);
+            for (var item in data) {
+                console.log(item);
+            }
         }
     });
-
+    
     app.innerHTML = "may be contents here";
 }
 
-renderJson(jsonUrl);
+renderByJsonUrl(jsonUrl);
