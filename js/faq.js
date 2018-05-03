a1795decefda17b7380ece96b84190b8e1b30a27
Gihyeon Yang <dexterastin@gmail.com>
2018-05-04 05:50:05 +0900

    feat: somul 2018

--- js/faq.js
+++ js/faq.js
@@ -1,15 +1,14 @@
-
 var divLinkContent = document.getElementById("link-content");
-var divContent     = document.getElementById("content");
+var divContent = document.getElementById("faq-content");
 
-var jsonUrl = "web-inf/faq.json";
+var jsonUrl = "https://raw.githubusercontent.com/somul-project/somul-project.github.io/master/web-inf/faq-new.json";
 
-var getJSON = function(url, callback) {
+var getJSON = function (url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
     xhr.responseType = 'json';
 
-    xhr.onload = function() {
+    xhr.onload = function () {
         var status = xhr.status;
         if (status === 200) {
             callback(null, xhr.response);
@@ -20,16 +19,32 @@ var getJSON = function(url, callback) {
     xhr.send();
 };
 
+function textToSmallTitle(num, text) {
+    var k =
+        '<div class="faq-small-title">' +
+        '<div class="faq-samll-tile-img">' +
+        '<img style="width: 33px; height: 63px; margin: 16px 21px 16px 101px;" src="./resources/faq-bottom-' + num + '.png">' +
+        '</div>' +
+        '<div class="faq-small-title-text">' +
+        '<div class="vcenter">' +
+        '<h2 class="faq-h2t">' + text + '</h2>' +
+        '</div>' +
+        '</div>' +
+        '</div>'
+    return k;
+
+}
+
 function textToTitleText(text) {
-    return "<h2>" + text + "</h2>";
+    return "<h2 class='faq-h2'>" + text + "</h2>";
 }
 
 function textToSubTitleText(text) {
-    return "<h3>" + text + "</h3>";
+    return "<h3 class='faq-h3'>" + text + "</h3>";
 }
 
 function textToPlainText(text) {
-    return "<p>" + text + "</p>"
+    return "<p class='faq-p'>" + text + "</p>"
 }
 
 function prefixAppendIndex(text, index) {
@@ -37,7 +52,7 @@ function prefixAppendIndex(text, index) {
 }
 
 function prefixAppendLinkIdAndIndex(text, linkId, index) {
-    return '<div id="' + linkId + (index + 1) + '" style="position:relative;top:-20px;visibility:hidden"></div>' 
+    return '<div id="' + linkId + (index + 1) + '" style="position:relative;top:-20px;visibility:hidden"></div>'
         + textToSubTitleText(linkId + "." + (index + 1) + " " + text);
 }
 
@@ -51,32 +66,32 @@ function getMoveText(name, linkId, index) {
 }
 
 function renderByJsonUrl(url) {
-    getJSON(url, function(err, data) {
+    var imageNum = 0;
+    getJSON(url, function (err, data) {
         if (err !== null) {
         } else {
             for (var item in data) {
-                
-                divLinkContent.innerHTML += textToTitleText(data[item].name);
+                imageNum++;
+                divLinkContent.innerHTML += textToSmallTitle(imageNum, data[item].name);
 
                 var linkText = "<ul>"
 
                 if (data[item].type == "link") {
-                    data[item].lists.forEach(function(item, index) {
+                    data[item].lists.forEach(function (item, index) {
                         linkText += getLinkText(item.name, item.link);
                     });
                 } else if (data[item].type == "move") {
-                    
-                    divContent.innerHTML += "<br /><hr><br />";
+
+                    divContent.innerHTML += "<br /><br />";
                     divContent.innerHTML += textToTitleText(data[item].name);
                     divContent.innerHTML += textToPlainText(data[item].description);
-                    divContent.innerHTML += "<br />";
                     var linkId = data[item].linkId;
-                    data[item].lists.forEach(function(item, index) {
-                        
+                    data[item].lists.forEach(function (item, index) {
+
                         linkText += getMoveText(item.name, linkId, index);
 
-                        divContent.innerHTML += prefixAppendLinkIdAndIndex(item.name, linkId, index); 
-                        divContent.innerHTML += textToPlainText(item.content); 
+                        divContent.innerHTML += prefixAppendLinkIdAndIndex(item.name, linkId, index);
+                        divContent.innerHTML += textToPlainText(item.content);
                     });
                 }
 
@@ -87,4 +102,6 @@ function renderByJsonUrl(url) {
     });
 }
 
-renderByJsonUrl(jsonUrl);
+$(document).ready(function () {
+    renderByJsonUrl(jsonUrl);
+});
